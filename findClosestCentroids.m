e9654ec25b19fc3f315588c7683b8331fe390beb
shanmu9898 <shanmu9898@gmail.com>
2018-01-05 17:16:39 +0530

    Implemented K means Clustering and PCA
    This is an unsupervised machine learning algorithm

---
+++ findClosestCentroids.m
@@ -0,0 +1,43 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+N = size(X,2);
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+for i=1:length(X)
+	for k=1:K		
+		for n=1:N	
+			a(n)=(X(i,n)-centroids(k,n)).^2;	
+		dist(k)=sum(a);		
+	end
+	[r,c] = min(dist);		
+	idx(i) = c;				
+end
+
+
+
+
+
+
+
+
+% =============================================================
+
+end
+
